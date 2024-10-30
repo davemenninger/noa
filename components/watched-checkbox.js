e65e61a503d0857e28ff8712ac71e26798d675c7
Dave Menninger <dave.menninger@gmail.com>
2024-10-29 21:34:13 -0400

    save/load watchlist in indexeddb

--- components/watched-checkbox.js
+++ components/watched-checkbox.js
@@ -5,11 +5,43 @@ class WatchedCheckbox extends HTMLElement {
   }
 
   update() {
+    var foo = false;
+
+    const request = window.indexedDB.open("mydatabase", 2);
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      const watchList = db.createObjectStore("watchList", { keyPath: "id" });
+
+      const request = watchList.get(this.id);
+      request.onsuccess = () => {
+        if (request.result) {
+          this.checked = request.result.watched;
+        }
+      };
+    };
+
+    request.onsuccess = (event) => {
+      const db = event.target.result;
+      const transaction = db.transaction(["watchList"], "readonly");
+      const watchList = transaction.objectStore("watchList");
+
+      const request = watchList.get(this.id);
+      request.onsuccess = () => {
+        if (request.result) {
+          // TODO: hacky
+          this.checked = request.result.watched;
+          var c = this.children[0].children[0];
+          c.checked = this.checked;
+        }
+      };
+    };
+
     const d = this.querySelector("div");
     d.setAttribute("class", "watched-checkbox");
     const checkbox = document.createElement("input");
     checkbox.setAttribute("id", this.getAttribute("id"));
     checkbox.setAttribute("type", "checkbox");
+    checkbox.checked = this.checked;
     checkbox.innerText = this.getAttribute("id");
     checkbox.addEventListener("click", this.mark);
     d.appendChild(checkbox);
@@ -18,8 +50,21 @@ class WatchedCheckbox extends HTMLElement {
   }
 
   mark(_) {
-    console.log("checked:", this.checked);
-    console.log("id:", this.id);
+    const request = window.indexedDB.open("mydatabase", 2);
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      const watchList = db.createObjectStore("watchList", { keyPath: "id" });
+
+      watchList.put({ id: this.id, watched: this.checked });
+    };
+
+    request.onsuccess = (event) => {
+      const db = event.target.result;
+      const transaction = db.transaction(["watchList"], "readwrite");
+      const watchList = transaction.objectStore("watchList");
+
+      watchList.put({ id: this.id, watched: this.checked });
+    };
   }
 }
 
