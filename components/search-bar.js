72d7353db07969d1462b6bf05361912a8b3cd2ec
Dave Menninger <dave.menninger@gmail.com>
2025-02-15 21:22:18 -0500

    add BBY/ABY search

--- components/search-bar.js
+++ components/search-bar.js
@@ -4,20 +4,23 @@ class SearchBarComponent extends HTMLElement {
             <form>
                 <label for="query">Q:</label>
                 <input type="text" id="query" name="query" />
+                <input type="text" id="bby" name="bby" />
                 <button type="submit">Search</button>
             </form>
         `;
 
     this.querySelector("form").onsubmit = (e) => {
       var q = e.target["0"].value;
+      var bby = e.target["1"].value;
       console.log(q);
+      console.log(bby);
       e.preventDefault();
 
       var db = document.getElementById("db");
       var results = document.getElementById("results");
       try {
         results.innerHTML = "";
-        var sql = "select * from media where title like '%" + q + "%' limit 9";
+        var sql = "select * from media where title like '%" + q + "%' and date like '"+bby+"%' limit 39";
         console.log(sql);
         db.query(sql, results);
       } catch (e) {
